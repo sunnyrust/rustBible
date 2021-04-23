fcf9406b12ccba9fd9a1c0ea904ec30bb5a1d9bb
sunny <qi.cao@cloudminds.com>
2021-04-23 18:26:54 +0800

    :+1:

--- src/chapter05/sunny_test01/adder/src/lib.rs
+++ src/chapter05/sunny_test01/adder/src/lib.rs
@@ -1,22 +1,61 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+#[allow(dead_code)]
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    // #[test]
+    // fn it_works() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+
+    // #[test]
+    // fn exploration() {
+    //     assert_eq!(2 * 2, 4);
+    // }
+
+    // #[test]
+    // fn do_fail() {
+    //     assert_eq!(2 -2, 4);
+    // }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail");
+    // }
+
+
+    use super::*;
 
     #[test]
-    fn exploration() {
-        assert_eq!(2 * 2, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 
-    #[test]
-    fn do_fail() {
-        assert_eq!(2 -2, 4);
-    }
 
     #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
+
 }
