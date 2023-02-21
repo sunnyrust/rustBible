b209032e2b0b4e3d527fc74438efb5c5163c05a1
Sunny <jinheking@gmail.com>
2023-02-21 16:18:24 +0800

    pgdb

--- src/07/helloworld-axum/src/controller/mod.rs
+++ src/07/helloworld-axum/src/controller/mod.rs
@@ -5,3 +5,11 @@ pub mod tag;
 fn get_web_info<'a>(state: &'a WebInfo) -> WebInfo{
    state.to_owned()
 }
+/// 渲染模板
+fn render<T: Template>(tpl: T, handler_name: &str) -> Result<super::vulcan_util::types::HtmlResponse> {
+   let out = tpl
+       .render()
+       .map_err(AppError::from)
+       .map_err(log_error(handler_name))?;
+   Ok(Html(out))
+}
